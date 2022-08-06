894961833d1eb6551a21950162317bca3c8a36d4
Jared Daniel <jareddaniel95@gmail.com>
2022-08-06 14:57:07 -0400

    Add other tables and category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,9 +20,7 @@ router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
-    where: {
-      id: req.params.id
-    },
+    where: {id: req.params.id},
     attributes: ['id', 'category_name'],
     include: [{
       model: Product,
@@ -41,17 +39,37 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   Category.create({
-    name: req.body.name
+    category_name: req.body.category_name
   }).then(data => res.json(data))
   .catch(err => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {id: req.params.id}
+  }).then(data => {
+    if(data[0] == null) {
+      res.status(404).json({message: 'Not found'});
+      return;
+    } else {
+      res.json(data);
+    }
+  }).catch(err => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  }).then(data => {
+    if(data == null) {
+      res.status(404).json({message: 'Not found'});
+      return;
+    } else {
+      res.json(data);
+    }
+  }).catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
